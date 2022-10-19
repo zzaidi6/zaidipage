912b7b2032613bcd0f03bf69942d51a33a6338f2
zzaidi6 <86411837+zzaidi6@users.noreply.github.com>
2022-10-19 22:11:24 +0530

    iphost.py

---
+++ iphost.py
@@ -0,0 +1,35 @@
+#colors
+red=fg('red')
+green=fg('green')
+blue=fg('blue')
+
+#banner
+print(green)
+print("-----Welcome to the ZaidiShell!------")
+print(red)
+print("type run to start and then,")
+print("type domain in the entry box.")
+print(green)
+
+#main script
+def code():
+  print(red)
+  ip = input("domain: ")
+  host = socket.gethostbyname(ip)
+  print(green)
+  print(host+ " is the ip address of " +ip)
+  print(blue)
+  print("url:- http://"+host)
+  print(green)
+
+#shell script
+def shell():
+  entry=input("ZaidiShell@=> ")
+  if "run" == entry :
+       code()
+  else :
+       print(red+"error: wrong argument!")
+       print(green)
+       return shell()
+
+shell()
